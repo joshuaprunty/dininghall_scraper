02e431a6b5f4ccf0bc8c5da12a6877f56a0a8e46
Josh Prunty <joshprunty@gmail.com>
2023-10-01 20:17:44 -0500

    First full version, with support for all dining halls and meals via user input

--- Dining_Hall_Webscraper.py
+++ Dining_Hall_Webscraper.py
@@ -1,20 +1,81 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
-import requests
 
-def Scrape():  
+def Scrape(hall, meal):  
 
-    retMessage = []
-
+    # DineOnCampus main URL
     url = 'https://dineoncampus.com/northwestern/whats-on-the-menu'
 
+    # Setup webdriver and wait for page to load
     driver = webdriver.Chrome()
-
     driver.get(url)
+    time.sleep(12)
 
-    time.sleep(15)
+    # Fetch the dropdown menu of dining halls
+    hallsDiv = driver.find_element(By.ID, 'menu-location-selector')
+    hallsDropdown = hallsDiv.find_element(By.CSS_SELECTOR, ":nth-child(2)")
 
+    # Click the dropdown button to open the menu
+    hallsButton = driver.find_element(By.ID, 'menu-location-selector__BV_toggle_')
+    hallsButton.click()
+    time.sleep(2)
+
+    # Select the left main dropdown containing dining hall names
+    locationsInnerList = hallsDropdown.find_element(By.CSS_SELECTOR, ":nth-child(1)")
+    mainUL = locationsInnerList.find_element(By.CSS_SELECTOR, ":nth-child(2)")
+
+    # Assign to variables each of the <li> elements for the various dining halls
+    allisonLI = mainUL.find_element(By.CSS_SELECTOR, ":nth-child(1)")
+    sargentLI = mainUL.find_element(By.CSS_SELECTOR, ":nth-child(2)")
+    fosterWestLI = mainUL.find_element(By.CSS_SELECTOR, ":nth-child(3)")
+    fosterEastLI = mainUL.find_element(By.CSS_SELECTOR, ":nth-child(4)")
+    elderLI = mainUL.find_element(By.CSS_SELECTOR, ":nth-child(5)")
+
+    # Now assign to variables the button elements themselves
+    allisonButton = allisonLI.find_element(By.CSS_SELECTOR, ":nth-child(1)")
+    sargentButton = sargentLI.find_element(By.CSS_SELECTOR, ":nth-child(1)")
+    fosterWestButton = fosterWestLI.find_element(By.CSS_SELECTOR, ":nth-child(1)")
+    fosterEastButton = fosterEastLI.find_element(By.CSS_SELECTOR, ":nth-child(1)")
+    elderButton = elderLI.find_element(By.CSS_SELECTOR, ":nth-child(1)")
+
+    # Switch statement to click and navigate to the appropriate dining hall page via function parameter passed
+    match hall:
+        case 'allison':
+            allisonButton.click()
+        case 'sargent':
+            sargentButton.click()
+        case 'fosterWest':
+            fosterWestButton.click()
+        case 'fosterEast':
+            fosterEastButton.click()
+        case 'elder':
+            elderButton.click()
+
+    # Wait for page load
+    time.sleep(5)
+
+    # Assign to variables the tab buttons for each meal
+    if hall == 'allison' or hall == 'sargent':
+        breakfastA = driver.find_element(By.XPATH, "//*[text()='Breakfast']")
+    
+    lunchA = driver.find_element(By.XPATH, "//*[text()='Lunch']")
+    dinnerA = driver.find_element(By.XPATH, "//*[text()='Dinner']")
+
+    # Switch statement to click appropriate meal button
+    match meal:
+        case 'breakfast':
+            breakfastA.click()
+        case 'lunch':
+            lunchA.click()
+        case 'dinner':
+            dinnerA.click()
+
+    # Wait for page load
+    time.sleep(10)
+
+    # Get html for BeautifulSoup scraping
     content = driver.page_source
 
     with open('output.html', 'w', encoding='utf-8') as f:
@@ -23,35 +84,44 @@ def Scrape():
     with open('output.html','r') as f:
         doc = BeautifulSoup(f, 'html.parser')
 
+    # Print out text of each <strong> tag - these contain the entree/option names
     tags = doc.find_all('strong')
-
-    with open('/Users/joshprunty/Desktop/python/Webscraper/dishes.txt','w') as f:
-        for tag in tags:
-            f.write(tag.text)
-            f.write('\n')
-
     for tag in tags:
-        retMessage.append(tag.text)
+        print(tag.text)
+
 
     driver.quit()
 
-    return retMessage
+# Expected strings for each input, for error catching. 'breakfast' is added to mealExpectedStrings
+# If the selected hall offers breakfast
+hallExpectedStrings = ["allison", "sargent", "fosterWest", "fosterEast", "elder"]
+mealExpectedStrings = ['lunch', 'dinner']
 
-def send_message(message):
-  resp = requests.post('http://textbelt.com/text', {
-    'phone': '14403713543',
-    'message': message,
-    'key': 'textbelt'
-  })
-  print(resp.json())
+# Loop to catch invalid inputs when getting user hall choice
+while True:
+    chosenHall = input("Please enter a dining hall (Type Exactly one of the following: allison, sargent, fosterWest, fosterEast, or elder) ")
+    if chosenHall in hallExpectedStrings:
+        break
+    else:
+        print("Invalid input. Please enter exactly one of the listed strings")
 
-greeting = "Good Morning! Here are today's dishes at Allison Dining Hall: \n\n"
-dishes = ""
-dshsList = Scrape()
-for dish in dshsList[0:3]:
-    dishes += dish
-    dishes += "\n"
-msg = greeting + dishes
+# mealPrompt variable, to be altered to include breakfast if relevant
+mealPrompt = 'Please enter a meal (Type Exactly one of the following: lunch or dinner)'
 
-send_message(msg)
+# Add breakfast as an option if chosen hall offers it 
+if chosenHall == 'allison' or chosenHall == 'sargent':
+    mealExpectedStrings.append('breakfast')
+    mealPrompt = 'Please enter a meal (Type Exactly one of the following: breakfast, lunch, or dinner)'
 
+# Similar loop to prompt for user input with error catches
+while True:
+    chosenMeal = input(mealPrompt)
+    if chosenMeal in mealExpectedStrings:
+        print("Fetching meal options...")
+        break
+    else:
+        print("Invalid input. Please enter exactly one of the listed strings")
+
+
+# Call the Scrape function
+Scrape(chosenHall, chosenMeal)
